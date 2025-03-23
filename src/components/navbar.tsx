9ef476680636dc6f980b2d39c51762f68422a3bb
RicoSawaya <ricosawaya@gmail.com>
2025-03-23 16:35:14 -0700

    changed path and link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,10 +21,10 @@ function NavBar(){
                         <Link to = "/" className="link">Rico Sawaya</Link>
                     </div>
                     <div className="right-nav">
-                        <Link to='/' className="link">Home</Link> 
-                        <Link to='/projects' className="link">Projects</Link> 
-                        <Link to='/studies' className="link">Studies</Link> 
-                        <Link to='/lifting' className="link">Lifting</Link>
+                        <Link to='/rico-portfolio/' className="link">Home</Link> 
+                        <Link to='/rico-portfolio/projects' className="link">Projects</Link> 
+                        <Link to='/rico-portfolio/studies' className="link">Studies</Link> 
+                        <Link to='/rico-portfolio/lifting' className="link">Lifting</Link>
                     </div>
                     <button className="grip-lines" onClick={toggleComponent}>
                         <img src={griplines}></img>
